b0e77098a202fdede0f7ce9e7e730f4941baa596
Byron Wang <byronwang123919@gmail.com>
2022-08-25 00:25:04 -0600

    update prettier

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
-import React from "react"
-import { Box, Button } from "@chakra-ui/react"
+import React from 'react';
+import { Box, Button } from '@chakra-ui/react';
+
+const navItems = ['About', 'Experience', 'Projects', 'Fun', 'Contact Me'];
 
 const Navbar = () => {
-  return <Box>This is the nav</Box>
-}
+  return <Box>This is the nav</Box>;
+};
 
-export default Navbar
+export default Navbar;

51b24e17dba8c403dc1d3a6eb641e5209c7e20a8
Byron Wang <byronwang123919@gmail.com>
2022-08-27 00:58:46 -0600

    added main components to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import Layout from '../components/Layout';
+import About from '../components/About';
+import Experience from '../components/Experience';
+import Projects from '../components/Projects';
+import Fun from '../components/Fun';
+import ContactMe from '../components/ContactMe';
+import Intro from '../components/Intro';
 
 export default function Home() {
   return (
     <Layout>
-      <Flex>Hello</Flex>
+      <Flex
+        height="100%"
+        direction="column"
+        alignItems="center"
+      >
+        <About />
+        <Intro />
+        <Experience />
+        <Projects />
+        <Fun />
+        <ContactMe />
+      </Flex>
     </Layout>
   );
 }

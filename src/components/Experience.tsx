4267b9f12c960ca1c7ef57558445c90b2451dc14
Byron Wang <byronwang123919@gmail.com>
2022-09-25 01:50:50 -0700

    initial work on card layout

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,8 +1,35 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import HeaderText from './HeaderText';
+import { graphql, useStaticQuery } from 'gatsby';
+import ExperienceCard from './ExperienceCard';
+
+const query = graphql`
+  query {
+    allContentfulExperience {
+      nodes {
+        childContentfulExperienceContentJsonNode {
+          description
+          tags
+        }
+        childContentfulExperienceLinkTextNode {
+          link
+        }
+        id
+        title
+        childContentfulExperienceLocationTextNode {
+          location
+        }
+      }
+    }
+  }
+`;
 
 const Experience = ({ id }) => {
+  const data = useStaticQuery(query);
+  const experiences = data.allContentfulExperience.nodes;
+  console.log(experiences, ' is experiences');
+
   return (
     <Flex
       id={id}
@@ -11,10 +38,12 @@ const Experience = ({ id }) => {
       mt="50px"
       mb="50px"
       height="auto"
+      flexDirection="column"
       justifyContent="center"
       width={{ base: '390px', md: '700px', lg: '900px' }}
     >
       <HeaderText text="Experience" />
+      <ExperienceCard />
     </Flex>
   );
 };

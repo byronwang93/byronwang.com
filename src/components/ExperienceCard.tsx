9c77425a70a331de3289faaed4fd840c2653c268
Byron Wang <byronwang123919@gmail.com>
2022-09-26 09:02:38 -0700

    added props to experiencecard

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,7 +1,30 @@
+import { Flex, Text } from '@chakra-ui/react';
 import React from 'react';
+import Tag from './Tag';
 
-const ExperienceCard = () => {
-  return <div>ExperienceCard</div>;
+interface ExperienceProps {
+  title: string;
+  link: string;
+  location: string;
+  description: string[];
+  tags: string[];
+}
+
+const ExperienceCard = ({
+  title,
+  link,
+  location,
+  description,
+  tags,
+}: ExperienceProps) => {
+  return (
+    <Flex flexDirection="column">
+      <Text>{title}</Text>
+      {tags.map((tag, id) => {
+        return <Tag />;
+      })}
+    </Flex>
+  );
 };
 
 export default ExperienceCard;

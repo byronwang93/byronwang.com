775dbddb530c4e471e563059c1c87dedfabc8e55
Byron Wang <byronwang123919@gmail.com>
2022-09-28 23:15:21 -0700

    initial project card layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,20 +1,62 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import HeaderText from './HeaderText';
+import { graphql, useStaticQuery } from 'gatsby';
+import ProjectCard from './ProjectCard';
+
+const query = graphql`
+  query {
+    allContentfulProjects {
+      nodes {
+        childContentfulProjectsContentJsonNode {
+          description
+          tags
+        }
+        childContentfulProjectsGithubLinkTextNode {
+          githubLink
+        }
+        date
+        title
+      }
+    }
+  }
+`;
 
 const Projects = ({ id }) => {
+  const data = useStaticQuery(query);
+  const projects = data.allContentfulProjects.nodes.reverse();
+
   return (
     <Flex
       id={id}
-      backgroundColor="lightblue"
       pt="20px"
       mt="50px"
       mb="50px"
       height="auto"
       justifyContent="center"
       width={{ base: '390px', md: '700px', lg: '900px' }}
+      flexDirection="column"
     >
       <HeaderText text="Projects" />
+      {projects.map((project, id) => {
+        const title = project.title;
+        const link =
+          project.childContentfulProjectsGithubLinkTextNode.githubLink;
+        const date = project.date;
+        const description =
+          project.childContentfulProjectsContentJsonNode.description;
+        const tags = project.childContentfulProjectsContentJsonNode.tags;
+        return (
+          <ProjectCard
+            date={date}
+            tags={tags}
+            description={description}
+            title={title}
+            link={link}
+            key={id}
+          />
+        );
+      })}
     </Flex>
   );
 };

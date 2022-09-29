8c90746f067d5e156291aa48f424fe28b9752ef3
Byron Wang <byronwang123919@gmail.com>
2022-09-28 23:42:42 -0700

    initial work on fun page section

--- src/components/Fun.tsx
+++ src/components/Fun.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 import HeaderText from './HeaderText';
 
@@ -7,16 +7,32 @@ const Fun = ({ id }) => {
     <Flex
       flexDirection="column"
       id={id}
-      backgroundColor="red"
+      backgroundColor="beige"
       pt="20px"
       mt="50px"
       mb="50px"
       height="auto"
       justifyContent="center"
       width={{ base: '390px', md: '700px', lg: '900px' }}
+      alignItems="center"
     >
       <HeaderText text="Fun" />
-      <Text></Text>
+      <Box
+        width={{ base: '200px', md: '400px', lg: '600px' }}
+        fontSize="18px"
+      >
+        <Text pb="10px">
+          Ever since I was a child, I’ve always had a passion for drawing,
+          spending countless hours drawing comics with my brother and creating
+          our own (admittingly simple) worlds.
+        </Text>
+        <Text>
+          From the theme of my website, you can probably tell I haven’t grown
+          out of the doodling phase, so a goal for this year is to learn how to
+          animate! If I end up following through, here’s where I’ll be linking
+          the videos so keep checking back for future updates!
+        </Text>
+      </Box>
     </Flex>
   );
 };

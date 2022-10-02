a20006960ee9ea5ca40737ac131c9aaac3d22e51
Byron Wang <byronwang123919@gmail.com>
2022-10-01 18:13:16 -0700

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: '@chakra-ui/gatsby-plugin',
       options: {
         /**

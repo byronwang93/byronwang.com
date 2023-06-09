26035223510cbe23cef32aeedca50f32954f0ee8
Byron Wang <byronwang123919@gmail.com>
2023-06-09 00:12:54 -0700

    removed contentful data to within the code

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,34 +1,48 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import HeaderText from './HeaderText';
-import { graphql, useStaticQuery } from 'gatsby';
 import ExperienceCard from './ExperienceCard';
 
-const query = graphql`
-  query {
-    allContentfulExperience {
-      nodes {
-        childContentfulExperienceContentJsonNode {
-          description
-          tags
-        }
-        childContentfulExperienceLinkTextNode {
-          link
-        }
-        id
-        title
-        childContentfulExperienceLocationTextNode {
-          location
-        }
-      }
-    }
-  }
-`;
+const experiences = [
+  // {
+  //   title: '',
+  //   location: '',
+  //   link: '',
+  //   description: [],
+  //   tags: [],
+  // },
+  {
+    title: 'Software Developer Co-op @ PayByPhone',
+    location: 'Vancouver, BC, May 2023 - Present',
+    link: 'https://www.paybyphone.com/',
+    description: [],
+    tags: ['JavaScript', 'React'],
+  },
+  {
+    title: 'Web Developer Co-op @ Apryse',
+    location: 'Vancouver, BC, May 2022 - Dec 2022',
+    link: 'https://www.pdftron.com/',
+    description: [
+      'Developed reusable and accessible UI components using TypeScript React and maintained code with production of end to end tests',
+      'Worked on integrating serverside features with our backend api to process user data which included improving user authentication',
+      'Utilizing agile scrum methodology to complete 2-week long sprints working on xodo.com with 2,000,000+ monthly users',
+    ],
+    tags: ['TypeScript', 'React', 'Next.js', 'Web Development'],
+  },
+  {
+    title: 'Undergraduate Teaching Assistant @ UBC',
+    location: 'Vancouver, BC, Sept 2020 - Dec 2021',
+    link: 'https://www.cs.ubc.ca/',
+    description: [
+      'Provided thought-provoking assistance to 600+ non-computer science students to help them understand computational concepts',
+      'Led weekly lab sessions for over 90 students, completing over 36 labs over the course of the term and prepared recorded video solutions to lab problems for success of future TAs and students',
+      'Hosted 10 office hours and a group review session before exams assisting 35+ students',
+    ],
+    tags: ['Algorithms', 'Teaching', 'Leadership'],
+  },
+];
 
 const Experience = ({ id }) => {
-  const data = useStaticQuery(query);
-  const experiences = data.allContentfulExperience.nodes.reverse();
-
   return (
     <Flex
       id={id}
@@ -45,12 +59,10 @@ const Experience = ({ id }) => {
       <HeaderText text="Experience" />
       {experiences.map((experience, id) => {
         const title = experience.title;
-        const link = experience.childContentfulExperienceLinkTextNode.link;
-        const location =
-          experience.childContentfulExperienceLocationTextNode.location;
-        const description =
-          experience.childContentfulExperienceContentJsonNode.description;
-        const tags = experience.childContentfulExperienceContentJsonNode.tags;
+        const link = experience.link;
+        const location = experience.location;
+        const description = experience.description;
+        const tags = experience.tags;
         return (
           <ExperienceCard
             link={link}
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,31 +1,69 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import HeaderText from './HeaderText';
-import { graphql, useStaticQuery } from 'gatsby';
 import ProjectCard from './ProjectCard';
 
-const query = graphql`
-  query {
-    allContentfulProjects {
-      nodes {
-        childContentfulProjectsContentJsonNode {
-          description
-          tags
-        }
-        childContentfulProjectsGithubLinkTextNode {
-          githubLink
-        }
-        date
-        title
-      }
-    }
-  }
-`;
+const projects = [
+  // {
+  //   title: "",
+  //   githubLink: "",
+  //   date: "",
+  //   description: [],
+  //   tags: []
+  // },
+  {
+    title: 'Linkt',
+    githubLink:
+      'https://chrome.google.com/webstore/detail/linkt/fjhmcdeacfacahfiiimnkmccnocidjpn/related',
+    date: 'July 2023',
+    description: [
+      'Developed a chrome extension to remove the hassle of repeatedly searching up the same links',
+      'Utilized local storage to persist data upon refresh',
+    ],
+    tags: ['React', 'Local Storage', 'Productivity'],
+  },
+  {
+    title: 'RiseOrRegret',
+    githubLink: 'https://github.com/byronwang93/RiseOrRegret',
+    date: 'July 2023',
+    description: [
+      'A practical alarm clock that incentivizes you to actually wake up',
+      'If you fail to turn off your alarm in time, the app sends a risky text to someone in your contacts >:)',
+      'Created for StormHacks 2023',
+    ],
+    tags: ['React Native', 'Twilio', 'Express.js'],
+  },
+  {
+    title: 'Portfolio Website',
+    githubLink: 'https://github.com/byronwang93/byronwang.com',
+    date: 'Sept 2022',
+    description: [
+      "The site you're currently looking at! Rendition 2 of my portfolio website, this is to showcase the progression of my frontend skills",
+    ],
+    tags: ['TypeScript', 'React', 'Gatsby', 'CSS', 'GraphQL'],
+  },
+  {
+    title: 'Recreation Centre Database Maintainer',
+    githubLink: 'https://github.com/yeojunh/304-Chimps',
+    date: 'April 2022',
+    description: [
+      'Utilized SQL and PHP to model a recreation centre that facilitates additions, deletes and updates of the application using entities and relationships',
+    ],
+    tags: ['PHP', 'SQL', 'Relational Databases'],
+  },
+  {
+    title: 'Personal Workout Generator',
+    githubLink: 'https://github.com/byronwang93/Workout-Generator-Java',
+    date: 'Aug 2021',
+    description: [
+      'Developed a workout generator, allowing users to input unique exercises and generates a random workout based off of user inputs and preferences',
+      'User inputs saved and loaded by converting data to JSON files',
+    ],
+    tags: ['Java', 'JSON'],
+  },
+];
 
 const Projects = ({ id }) => {
-  const data = useStaticQuery(query);
-  const projects = data.allContentfulProjects.nodes.reverse();
-
   return (
     <Flex
       id={id}
@@ -40,12 +78,10 @@ const Projects = ({ id }) => {
       <HeaderText text="Projects" />
       {projects.map((project, id) => {
         const title = project.title;
-        const link =
-          project.childContentfulProjectsGithubLinkTextNode.githubLink;
+        const link = project.githubLink;
         const date = project.date;
-        const description =
-          project.childContentfulProjectsContentJsonNode.description;
-        const tags = project.childContentfulProjectsContentJsonNode.tags;
+        const description = project.description;
+        const tags = project.tags;
         return (
           <ProjectCard
             date={date}

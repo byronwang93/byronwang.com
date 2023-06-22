117d6d08ab8fe875e95ffff026bdcb9b2ce8289f
Byron Wang <byronwang123919@gmail.com>
2023-06-22 00:11:49 -0700

    changed layout to include animation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Footer from '../components/Footer';
 import { Box, ChakraProvider, VStack } from '@chakra-ui/react';
 import theme from '../@chakra-ui/gatsby-plugin/theme';
 import 'normalize.css';
 import './layout.css';
+import LoadingScreen from './LoadingScreen';
 
 const Layout = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const targetRef = useRef(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+
+      // Check if there is a hash in the URL (e.g., #my-div) and scroll to the target
+      if (window.location.hash) {
+        const targetElement = document.querySelector(window.location.hash);
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    }, 3000); // Adjust the delay (in milliseconds) as desired
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
-      <VStack
-        display="flex"
-        w="100%"
-        minHeight="100vh"
-      >
-        {children}
-        <Box
-          pt="35px"
-          position="relative"
-          left="0"
-          bottom="20px"
-          right="0"
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <VStack
+          display="flex"
+          w="100%"
+          minHeight="100vh"
+          ref={targetRef}
         >
-          <Footer />
-        </Box>
-      </VStack>
+          {children}
+          <Box
+            pt="35px"
+            position="relative"
+            left="0"
+            bottom="20px"
+            right="0"
+          >
+            <Footer />
+          </Box>
+        </VStack>
+      )}
     </ChakraProvider>
   );
 };

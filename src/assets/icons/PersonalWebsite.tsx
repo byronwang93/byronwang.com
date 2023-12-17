de93b65a157ea795034f916b2786238658d0eea4
byronwang93 <byronwang123919@gmail.com>
2023-12-17 14:20:15 -0800

    personal website icon

--- src/assets/icons/PersonalWebsite.tsx
+++ src/assets/icons/PersonalWebsite.tsx
@@ -13,19 +13,21 @@ const SvgPersonalWebsite = (props: SVGProps<SVGSVGElement>) => (
       cx={32}
       cy={32}
       r={30}
-      stroke="#fff"
+      // stroke="#fff"
+      fill="none"
       strokeWidth={4}
     />
     <circle
       cx={32}
       cy={25}
       r={11}
-      stroke="#fff"
+      fill="none"
+      // stroke="#fff"
       strokeWidth={4}
     />
     <path
       d="M24 33.5s-4.698 5.382-6.5 9.5C14 51 15 57 15 57M39 33s4.698 5.382 6.5 9.5c3.5 8 2.5 14 2.5 14"
-      stroke="#fff"
+      // stroke="#fff"
       strokeWidth={4}
     />
   </svg>

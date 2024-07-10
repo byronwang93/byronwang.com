62d200191777be815928e74e254b8fad23e02d99
byronwang93 <byronwang123919@gmail.com>
2024-07-09 22:56:44 -0700

    another

--- src/components/Fun.tsx
+++ src/components/Fun.tsx
@@ -237,7 +237,7 @@ const Fun = ({ id }) => {
       tldr: 'aspiring chess scrub',
       notable: 'rapid rating of 600',
       description:
-        "2 years after watching the Queen's Gambit, I randomly found myself binge watching GothamChess on YouTube. Standly proudly with a meme elo, I've played my share of tough opponents (and lost everytime), but hopefully this is my villain origin story >:)",
+        "2 years after watching the Queen's Gambit, I randomly found myself binge watching GothamChess on YouTube. Standly proudly with a meme elo, I've played my share of tough opponents (and lost everytime), but hopefully this is my villain origin story ...",
       image: [],
       videoLink: null,
       caption: [],

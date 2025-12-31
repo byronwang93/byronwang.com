d1728f5d1a51e5f3488d7eeb5027ccc453668fe3
Byron Wang <byron.wang@astronomer.io>
2025-12-31 00:42:54 -0800

    clean up configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,12 +11,6 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        // Add any options here
-      },
-    },
-    {
       resolve: `gatsby-plugin-web-font-loader`,
       options: {
         google: {
